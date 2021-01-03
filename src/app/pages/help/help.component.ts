aa8df6fa5c90267f7a26eaf5d1f8f49a6043f501
nkadebug <nkadebug@gmail.com>
2021-01-03 22:30:27 +0530

    update

--- src/app/pages/help/help.component.ts
+++ src/app/pages/help/help.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-help',
   templateUrl: './help.component.html',
-  styleUrls: ['./help.component.scss']
+  styleUrls: ['./help.component.scss'],
 })
 export class HelpComponent implements OnInit {
-
-  constructor() { }
+  status: Observable<any> = of(null);
+  constructor(private api: ApiService) {}
 
   ngOnInit(): void {
+    this.status = this.api.get('status.json');
   }
-
 }
